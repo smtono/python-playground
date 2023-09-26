e09d7e0f06d292207b868db725d4ef07e9dfa746
tono || トノ <64560893+smtono@users.noreply.github.com>
2023-09-25 20:43:01 -0500

    e

---
+++ src/scripts/test.py
@@ -0,0 +1,21 @@
+"""
+Table Creation in docx
+"""
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
